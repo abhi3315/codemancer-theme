48030d54af56239af237c38396490f1348b396bb
Abhishek Sharma <abhishek3.dev@gmail.com>
2023-04-12 12:48:41 +0530

    feat: navigation styles

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
  */
 const fs = require('fs');
 const path = require('path');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
 
 /**
@@ -12,6 +13,12 @@ const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 
 // Extend the default config.
 const sharedConfig = {
+	...defaultConfig,
+	output: {
+		path: path.resolve(process.cwd(), 'assets', 'build', 'js'),
+		filename: '[name].js',
+		chunkFilename: '[name].js',
+	},
 	plugins: [
 		...defaultConfig.plugins.map((plugin) => {
 			if (plugin.constructor.name === 'MiniCssExtractPlugin') {
@@ -21,28 +28,36 @@ const sharedConfig = {
 		}),
 		new RemoveEmptyScriptsPlugin(),
 	],
+	optimization: {
+		...defaultConfig.optimization,
+		splitChunks: {
+			...defaultConfig.optimization.splitChunks,
+		},
+		minimizer: defaultConfig.optimization.minimizer.concat([
+			new CssMinimizerPlugin(),
+		]),
+	},
 };
 
 // Generate a webpack config which includes setup for CSS extraction.
 // Look for css/scss files and extract them into a build/css directory.
 const styles = {
+	...sharedConfig,
 	entry: () => {
 		const entries = {};
 
 		const dir = './assets/src/css';
 		fs.readdirSync(dir).forEach((fileName) => {
-			entries[fileName.replace(/\.[^/.]+$/, '')] = path.resolve(
-				dir,
-				fileName
-			);
+			const fullPath = `${dir}/${fileName}`;
+			if (!fs.lstatSync(fullPath).isDirectory()) {
+				entries[fileName.replace(/\.[^/.]+$/, '')] = fullPath;
+			}
 		});
 
 		return entries;
 	},
-	output: {
-		path: path.resolve(process.cwd(), 'build', 'css'),
-		filename: '[name].css',
-		chunkFilename: '[name].css',
+	module: {
+		...sharedConfig.module,
 	},
 	plugins: [
 		...sharedConfig.plugins.filter(
